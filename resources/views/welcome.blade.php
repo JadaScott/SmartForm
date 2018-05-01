337e9606c2663af310b1d0e3267c6eb530efa1cc
Jada Scott <jadalscott@hotmail.com>
2018-04-30 20:11:42 -0600

    Smart form

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Massively by HTML5 UP</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <link rel="stylesheet" href="/css/main.css" />
+        <noscript><link rel="stylesheet" href="/css/app.css" /></noscript>
+    </head>
+    <body class="is-loading">
+
+        <!-- Wrapper -->
+            <div id="wrapper" class="fade-in">
+
+                <!-- Intro -->
+                    <div id="intro">
+                        <h1>This is<br />
+                        Massively</h1>
+                        <p>A free, fully responsive HTML5 + CSS3 site template designed by <a href="https://twitter.com/ajlkn">@ajlkn</a> for <a href="https://html5up.net">HTML5 UP</a><br />
+                        and released for free under the <a href="https://html5up.net/license">Creative Commons license</a>.</p>
+                        <ul class="actions">
+                            <li><a href="#header" class="button icon solo fa-arrow-down scrolly">Continue</a></li>
+                        </ul>
+                    </div>
+
+                <!-- Header -->
+                    <header id="header">
+                        <a href="index.html" class="logo">Massively</a>
+                    </header>
+
+                <!-- Nav -->
+                    <nav id="nav">
+                        <ul class="links">
+                            <li class="active"><a href="/">This is Massively</a></li>
+                            <li><a href="/generic">Generic Page</a></li>
+                            <li><a href="/elements">Elements Reference</a></li>
+                        </ul>
+                        <ul class="icons">
+                            <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+                            <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+                            <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+                            <li><a href="#" class="icon fa-github"><span class="label">GitHub</span></a></li>
+                        </ul>
+                    </nav>
+
+                <!-- Main -->
+                    <div id="main">
+
+                       <form id="customerinfo" method="post" action="contactMailer.php">
+        
+<div class="container">
+        <div class="list-group-item">
+          <div class="list-group-item-heading">          
+              <div class="row">
+               
+             <div class="col-xs-9">  
+          <div class="form-group">
+          <label for="name">Name:</label>
+          <input type="text" name="name" class="form-control
+          form-control-large" id="name" placeholder="Enter name">
+          </div>
+          <div class="form-group">
+          <label for="email">Email:</label>
+          <input type="text" name="email" class="form-control form-control-large" id="email" placeholder="Enter email">
+          </div>
+        <label for="message">Message:</label>           
+      <div class="field">
+       
+        <textarea id="message" name="message" required></textarea>
+      </div>
+
+      <div class="field">
+        <button type="submit" onclick="redirect()">Send</button>
+      </div>
+     </form>
+
+    </div>
+  </div>
+</div>
+</div>
+</div>
+</div>
+
+  <script src="jquery-2.1.0.min.js"></script>
+  <script src="app.js"></script>
+
+
+                <!-- Footer -->
+                    <footer id="footer">
+                       
+                     
+                        </section>
+                    </footer>
+
+                <!-- Copyright -->
+                    <div id="copyright">
+                        <ul><li>&copy; 2018</li><li>Jada Scott <a href="#"</a></li></ul>
+                    </div>
+
+            </div>
+
+        <!-- Scripts -->
+            <script src="assets/js/jquery.min.js"></script>
+            <script src="assets/js/jquery.scrollex.min.js"></script>
+            <script src="assets/js/jquery.scrolly.min.js"></script>
+            <script src="assets/js/skel.min.js"></script>
+            <script src="assets/js/util.js"></script>
+            <script src="assets/js/main.js"></script>
+
+    </body>
+</html>
